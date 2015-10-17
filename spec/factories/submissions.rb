1ed2d84a3f433aa7ff8fb26ee306affbe8cc5785
Kieran Klaassen <kieranklaassen@gmail.com>
2015-10-17 13:29:27 -0500

    Add carrier wave

--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: submissions
+#
+#  id          :integer          not null, primary key
+#  file        :string
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryGirl.define do
   factory :submission do
     file "MyString"
