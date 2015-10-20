07cf5242c239a7d542b7391f5f6e4ca65f7b5222
shaista ansari <shaansari@hotmail.com>
2015-10-19 22:12:02 -0400

    Added config.assets.initialize_on_precompile = false to applications.rb file for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20151017210847) do
   add_index "submissions", ["user_id"], name: "index_submissions_on_user_id"
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -60,7 +61,6 @@ ActiveRecord::Schema.define(version: 20151017210847) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
